1dce4eed607b2289a1fffdd6fcc22afd108e9ecb
mrifatsyauqi <m.rifatsyauqii@gmail.com>
2026-08-27 16:11:59 +0700

    feat(frontend): load RuangKirim brand theme overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+import './ruangkirim-brand.css'
 import App from './App.tsx'
 
 const queryClient = new QueryClient({

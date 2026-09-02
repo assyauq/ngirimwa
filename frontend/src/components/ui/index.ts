acadb466f27ef56fc9ef8b3873fa1ac7d765c430
mrifatsyauqi <m.rifatsyauqii@gmail.com>
2026-09-02 23:37:49 +0700

    feat(ui): add shared component exports

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as EmptyState } from './EmptyState';
+export { default as ErrorState } from './ErrorState';
+export { default as Input } from './Input';
+export { default as LoadingState } from './LoadingState';
